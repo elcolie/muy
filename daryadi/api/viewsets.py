9fde3434ed3de6ca4c2759aa658f33f8f1ac1ece
elcolie <sarit@elcolie.com>
2019-12-20 19:27:43 +0700

    Debug

--- daryadi/api/viewsets.py
+++ daryadi/api/viewsets.py
@@ -14,12 +14,34 @@ class PuzzleMoneyViewSet(viewsets.ModelViewSet):
 
 class SimpleAPIViewSet(APIView):
 
-    def get(self, request, format=None):
-        return Response(data={
-            'message': "Get"
-        }, status=status.HTTP_200_OK)
-
     def post(self, request, format=None):
+        """
+        SCB callback example
+        {
+          'payeeProxyId': '625534193160056',
+          'payeeProxyType': 'BILLERID',
+          'payeeAccountNumber': '0987654321',
+          'payeeName': 'Ming Sama',
+          'payerProxyId': '0812345678',
+          'payerProxyType': 'MSISDN',
+          'payerAccountNumber': '1412890001',
+          'payerName': 'Juli Chugsakon',
+          'sendingBankCode': '014',
+          'receivingBankCode': '014',
+          'amount': '150',
+          'channelCode': 'PMH',
+          'transactionId': '20191220526NcwMVe3v1TcW',
+          'transactionDateandTime': '2019-12-20T19:12:03+07:00',
+          'billPaymentRef1': 'D72568249FC1432181FD',
+          'billPaymentRef2': '2FD70D4444064691A20B',
+          'billPaymentRef3': 'MIN',
+          'currencyCode': '764',
+          'transactionType': 'Domestic Transfer'
+        }
+        :param request:
+        :param format:
+        :return:
+        """
         return Response(data={
-            'message': "Post"
+            'message': "Good Day"
         }, status=status.HTTP_200_OK)
